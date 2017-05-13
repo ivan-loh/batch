d3dbddc2457aa644d051d3805e0b14c4acfb3440
1vt <ivan@tideanalytics.com>
2017-05-13 16:31:00 +0800

    initial

---
+++ Batch.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"time"
+)
+
+/**
+ * Reader
+ */
+
+type Reader interface {
+	open()
+	read() Record
+	close()
+}
+
+/**
+ * Processor
+ */
+
+type Processor interface {
+	processRecord(r Record) Record
+}
+
+/**
+ * Record
+ */
+
+type Record interface {
+	header() Header
+	payload() map[string]interface{}
+}
+
+type Header struct {
+	number       int64
+	source       string
+	creationDate time.Time
+}
+
+/**
+ * Writer
+ */
+
+type Writer interface {
+	open()
+	writeRecord(record Record)
+	close()
+}
+
+/**
+ * Batch Definition/Flow
+ */
+
+type Batch struct {
+	reader    Reader
+	processor Processor
+	writer    Writer
+}
+
+func (b *Batch) setReader(r Reader) Batch {
+	b.reader = r
+	return *b
+}
+
+func (b *Batch) setProcessor(p Processor) Batch {
+	b.processor = p
+	return *b
+}
+
+func (b *Batch) setWriter(w Writer) Batch {
+	b.writer = w
+	return *b
+}
+
+func (b *Batch) execute() {
+
+	reader := b.reader
+	processor := b.processor
+	writer := b.writer
+
+	reader.open()
+
+	record := reader.read()
+	for record {
+		writer.writeRecord(processor.processRecord(record))
+		record = reader.read()
+	}
+
+	reader.close()
+	writer.close()
+}
+
